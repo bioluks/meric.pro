b5e0b7b766a9fa5c833065bb9ba3a12c8bec8f91
bioluks <askinmeric79@gmail.com>
2022-10-25 22:06:10 +0200

    Enabled lazy image loading, changed criny text (EN), fixed the contact form by downgrading Firebase

---
+++ public/js/firebase-init.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCwqAAPfh-I8AkNWU_a_Fo3Igv_j3J209g",
+    authDomain: "sitehomepersonal.firebaseapp.com",
+    databaseURL: "https://sitehomepersonal.firebaseio.com",
+    projectId: "sitehomepersonal",
+    storageBucket: "sitehomepersonal.appspot.com",
+    messagingSenderId: "500257117750",
+    appId: "1:500257117750:web:2b660fd22abd0804c62a97"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
